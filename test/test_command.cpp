9365061480bb8260382622e732ec4c28e8566941
Kenji MINOURA <kenji@kandj.org>
2015-09-21 23:20:05 +0900

    untabify

--- test/test_command.cpp
+++ test/test_command.cpp
@@ -18,25 +18,25 @@ using namespace kjsd;
 
 static void static_arg0()
 {
-	cout << "I am static_arg0" << endl;
+    cout << "I am static_arg0" << endl;
 }
 
 static int static_arg1(int a)
 {
-	cout << "I am static_arg1: " << a << endl;
-	return a;
+    cout << "I am static_arg1: " << a << endl;
+    return a;
 }
 
 static int static_arg2(int a, int b)
 {
-	cout << "I am static_arg2: " << a << ',' << b << endl;
-	return a + b;
+    cout << "I am static_arg2: " << a << ',' << b << endl;
+    return a + b;
 }
 
 static int static_arg3(int a, int b, int c)
 {
-	cout << "I am static_arg3: " << a << ',' << b << ',' << c << endl;
-	return a + b + c;
+    cout << "I am static_arg3: " << a << ',' << b << ',' << c << endl;
+    return a + b + c;
 }
 
 namespace test_command_ns
@@ -101,49 +101,49 @@ static Bar b_;
 
 static const char* test_exec()
 {
-	Command<void()> c0;
-	Command<int(int)> c1;
-	Command<int(int, int)> c2(1, 2);
-	Command<int(int, int, int)> c3(1, 2, 3);
+    Command<void()> c0;
+    Command<int(int)> c1;
+    Command<int(int, int)> c2(1, 2);
+    Command<int(int, int, int)> c3(1, 2, 3);
 
-	c0 = new InstanceFunction<Foo, void()>(&f_, &Foo::arg0);
-	c0();
-	c1 = new InstanceFunction<Foo, int(int)>(&f_, &Foo::arg1);
+    c0 = new InstanceFunction<Foo, void()>(&f_, &Foo::arg0);
+    c0();
+    c1 = new InstanceFunction<Foo, int(int)>(&f_, &Foo::arg1);
     c1.bind(1);
     KJSD_CUNIT_ASSERT(c1() == 1);
-	c2 = new InstanceFunction<Foo, int(int, int)>(&f_, &Foo::arg2);
+    c2 = new InstanceFunction<Foo, int(int, int)>(&f_, &Foo::arg2);
     KJSD_CUNIT_ASSERT(c2() == 3);
-	c3 = new InstanceFunction<Foo, int(int, int, int)>(&f_, &Foo::arg3);
+    c3 = new InstanceFunction<Foo, int(int, int, int)>(&f_, &Foo::arg3);
     KJSD_CUNIT_ASSERT(c3() == 6);
 
-	Command<int(int, int, int)> cc3;
+    Command<int(int, int, int)> cc3;
     
     cc3();
     cc3 = c3;
     KJSD_CUNIT_ASSERT(cc3() == 6);
 
-	c0 = new InstanceFunction<Bar, void()>(&b_, &Bar::arg0);
-	c0();
-	c1 = new InstanceFunction<Bar, int(int)>(&b_, &Bar::arg1);
-	c1.bind(11);
+    c0 = new InstanceFunction<Bar, void()>(&b_, &Bar::arg0);
+    c0();
+    c1 = new InstanceFunction<Bar, int(int)>(&b_, &Bar::arg1);
+    c1.bind(11);
     KJSD_CUNIT_ASSERT(c1() == 11);
-	c2 = new InstanceFunction<Bar, int(int,int)>(&b_, &Bar::arg2);
-	c2.bind(11, 22);
+    c2 = new InstanceFunction<Bar, int(int,int)>(&b_, &Bar::arg2);
+    c2.bind(11, 22);
     KJSD_CUNIT_ASSERT(c2() == 33);
-	c3 = new InstanceFunction<Bar, int(int,int,int)>(&b_, &Bar::arg3);
-	c3.bind(11, 22, 33);
+    c3 = new InstanceFunction<Bar, int(int,int,int)>(&b_, &Bar::arg3);
+    c3.bind(11, 22, 33);
     KJSD_CUNIT_ASSERT(c3() == 66);
 
-	c0 = new StaticFunction<void()>(&static_arg0);
-	c0();
-	c1 = new StaticFunction<int(int)>(&static_arg1);
-	c1.bind(111);
+    c0 = new StaticFunction<void()>(&static_arg0);
+    c0();
+    c1 = new StaticFunction<int(int)>(&static_arg1);
+    c1.bind(111);
     KJSD_CUNIT_ASSERT(c1() == 111);
-	c2 = new StaticFunction<int(int,int)>(&static_arg2);
-	c2.bind(111, 222);
+    c2 = new StaticFunction<int(int,int)>(&static_arg2);
+    c2.bind(111, 222);
     KJSD_CUNIT_ASSERT(c2() == 333);
-	c3 = new StaticFunction<int(int,int,int)>(&static_arg3);
-	c3.bind(111, 222, 333);
+    c3 = new StaticFunction<int(int,int,int)>(&static_arg3);
+    c3.bind(111, 222, 333);
     KJSD_CUNIT_ASSERT(c3() == 666);
 
     return 0;
